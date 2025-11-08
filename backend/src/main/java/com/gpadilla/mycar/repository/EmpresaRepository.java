ca2d0b500e2f4b912bc973f1cde0ba101b2130cf
Agustin Olivares <faolivares3112@gmail.com>
2025-11-08 00:19:53 -0300

    añado entidades

---
+++ backend/src/main/java/com/gpadilla/mycar/repository/EmpresaRepository.java
@@ -0,0 +1,10 @@
+package com.gpadilla.mycar.repository;
+
+import com.gpadilla.mycar.entity.Empresa;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface EmpresaRepository extends BaseRepository<Empresa, String> {
+    Optional<Empresa> findByNombreIgnoreCase(String nombre);
+}
